c9b33b9ef9105e266d0576abb842f708acd0f4f0
Ilia Loviagin <ilia.loviagin@gmail.com>
2024-06-12 21:37:23 +0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ilia Loviagin on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
